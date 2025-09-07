beb615b4c6b5e6853da6eb86765c46066af41fc9
Nifla.R <nykedotnyc@gmail.com>
2025-09-07 21:50:35 +0530

    Delete Eventra directory

--- Eventra/Backend/update_event_plans_table.sql
+++
@@ -1,9 +0,0 @@
-USE eventra_esrs;
-
-ALTER TABLE event_plans 
-ADD COLUMN approval_documents JSON NULL 
-AFTER documents;
-
-UPDATE event_plans 
-SET approval_documents = NULL 
-WHERE approval_documents IS NULL; 
